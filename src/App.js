813f9e4ba17d333763f9b2e33c7ccc81b1ae6025
Andrew Whillans <Andrew.Whillans@gov.bc.ca>
2018-10-09 16:27:12 -0700

    Update home page text

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <section>
+          <br/>
+          <p>I am groot.</p>
+        </section>
       </div>
     );
   }
